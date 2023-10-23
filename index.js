5caaca139e58bd3ee10c5af199bef3275a59246d
lunapazfernanda <luna.gamero@gmail.com>
2023-10-23 10:45:34 -0300

    Finalizado

---
+++ index.js
@@ -0,0 +1,19 @@
+let Fibonacci = [0, 1];
+let num
+let noEsNumero = false;
+
+while (!noEsNumero) {
+  num = parseInt(prompt("Escriba un número: "));
+
+  if (!isNaN(num)) {
+    noEsNumero = true;
+  } else {
+    console.log("Error, escriba un número válido: ");
+  }
+}
+
+for (let i = 2; i <= num; i++) {
+  Fibonacci.push(Fibonacci[i - 1] + Fibonacci[i - 2]);
+}
+console.log(Fibonacci);
+
